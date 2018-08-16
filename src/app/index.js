f6d3b1b4d82316bfb4c1514a81ead798970a1b41
Edward Dupre <edwd42@gmail.com>
2018-08-16 09:07:30 -0400

    update index.js with createReactClass and edit README.md to reflect Reacts version 16 changes

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
-alert('It works!!!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
+
+//Create a component
+//var TodoComponent = React.createClass({
+var TodoComponent = createReactClass({
+    render: function() {
+        return ( 
+            <h1> Hello there React world! It works!</h1>
+        );
+    }
+});
+
+ReactDOM.render( <TodoComponent /> , document.getElementById('todo-wrapper'));
