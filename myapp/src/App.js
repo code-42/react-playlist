5c16e75da0501678b72c602f413854cff470a41c
Edward Dupre <edwd42@gmail.com>
2018-08-21 08:56:09 -0400

    completed lesson-15

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 
 class App extends Component {
   state = {
     ninjas: [
-      { name: 'Ryu', age: 30, belt: 'blackd', id: 1 },
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
       { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
   render() {
